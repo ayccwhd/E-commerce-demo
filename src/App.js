571740226e78d0aeb6641353355eb23d87caedaf
ayccwhd <49759683+ayccwhd@users.noreply.github.com>
2022-03-10 14:13:30 +0800

    Revert "登陆注册"

--- src/App.js
+++ src/App.js
@@ -1,72 +1,24 @@
-/*
- * @Author: aycc
- * @Date: 2022-03-01 18:22:14
- * @LastEditors: aycc
- * @LastEditTime: 2022-03-10 12:40:26
- * @Description: file content
- * @FilePath: \my-app\src\App.js
- */
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
-//R6的outlet
-import Layout from './pages/Layout';
-import Editor from './pages/Editor';
-import Admin from './pages/Admin';
-import Missing from './pages/Missing';
-//import Unauthorized from './pages/Unauthorized';
-import ShoppingCart from './pages/ShoppingCart';
-import LinkPage from './pages/LinkPage';
-//import RequireAuth from './pages/RequireAuth';
-import { Routes, Route } from 'react-router-dom';
-
-//todo:登陆和未登录的页面区别
-// const ROLES = {
-//   'User': 2001,
-//   'Editor': 1984,
-//   'Admin': 5150
-// }
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* 未登录页面 */}
-        <Route path="linkpage" element={<LinkPage />} />
-        <Route path="home" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-
-        {/* <Route path="unauthorized" element={<Unauthorized />} />  */}
-
-        <Route path="editor" element={<Editor />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="shoppingcart" element={<ShoppingCart />} />
-
-
-        {/* //登陆页面
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
-          <Route path="editor" element={<Editor />} />
-        </Route>
-
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-          <Route path="admin" element={<Admin />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
-          <Route path="shoppingcart" element={<ShoppingCart />} />
-        </Route> */}
-
-        {/* catch all */}
-        <Route path="*" element={<Missing />} />
-      </Route>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
-/*
- * @Author: aycc
- * @Date: 2022-03-09 18:22:14
- * @LastEditors: aycc
- * @LastEditTime: 2022-03-10 12:40:39
- * @Description: file content
- * @FilePath: \my-app\src\index.js
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom ';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

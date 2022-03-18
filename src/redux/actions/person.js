8032a6b1d8c05fafcbe7aa0c380197290d8fdf64
玉桂狗呀 <njaulx2017@126.com>
2022-03-18 23:40:22 +0800

    首页初版

--- src/redux/actions/person.js
+++
@@ -1,4 +0,0 @@
-import { ADD_PERSON } from '../constant'
-
-//创建增加一个人的动作对象
-export const createAddPersonAction = (personObj) => ({ type: ADD_PERSON, data: personObj })
--- src/redux/constant.js
+++
@@ -1,5 +0,0 @@
-// 该模块用于定义Action对象中type类型的常量值
-
-export const INCREMENT = 'increment'
-export const DECREMENT = 'decrement'
-export const ADD_PERSON = 'add_person'

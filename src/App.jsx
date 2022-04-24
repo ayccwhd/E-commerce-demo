a241daa26942344c83e69601e303d38233685818
ayccwhd <49759683+ayccwhd@users.noreply.github.com>
2022-04-24 19:55:28 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-import React, { Component } from 'react';
-import {Route,Switch,Redirect} from 'react-router-dom'
-
-import Pay from './pages/payment/Pay'
-import PayMent from './pages/payment/PayMent'
-import PayDown from './pages/payment/PayDown';
-
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Switch>
-                    <Route path="/pay" component={Pay}></Route>
-                    <Route path="/payment" component={PayMent}></Route>
-                    <Route path="/paydown" component={PayDown}></Route>
-                    <Redirect to="/pay"></Redirect>
-                    {/* <Route path="/payment" component={PayMent}></Route> */}
-                </Switch>
-                {/* <PayMent/> */}
-            </div>
-        );
-    }
-
-}
-
-
 import logo from './logo.svg';
 import Shop from './Components/Shop/Shop'
 import SearchResult from './Components/SearchResult/SearchResult'

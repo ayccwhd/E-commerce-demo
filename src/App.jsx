62bd0b4b67225b4a856d3f7b99169b1e63affaf2
ayccwhd <49759683+ayccwhd@users.noreply.github.com>
2022-04-24 19:54:48 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,49 @@ export default class App extends Component {
 
 }
 
+
+import logo from './logo.svg';
+import Shop from './Components/Shop/Shop'
+import SearchResult from './Components/SearchResult/SearchResult'
+import Home from './Components/Home/Home'
+import Cart from './views/Cart/Cart'
+import Me from './views/Me/Me'
+import Product from './views/Product/Product.jsx'
+import Shop from './views/Shop/Shop'
+import Pay from './pages/payment/Pay'
+import PayMent from './pages/payment/PayMent'
+import PayDown from './pages/payment/PayDown';
+import SearchField from './Components/SearchField/SearchField'
+import React, { Component } from 'react';
+import './App.css';
+import { Link, Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'
+import { HashRouter, Switch } from 'react-router-dom'
+import TabDivide from 'zent/es/tabs/components/TabDivide';
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/searchfield" element={<SearchField />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/me" element={<Me/>}/>
+        <Route path="/productinfo/:id" element={<Product/>}/>
+        <Route path="/shopinfo/:shopname" element={<Shop/>}/>
+        <Route path="/pay" element={<Pay/>}>
+        <Route path="/payment" element={<PayMent/>}>
+        <Route path="/paydown" element={<PayDown/>}>
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/searchresult" element={<SearchResult />} />
+        {/* <Route path="/test" element={<Test />} /> */}
+      </Routes>
+    </HashRouter>
+    // <div>
+    //   <SearchResult />
+    // </div>
+  );
+}
+
+export default App;
+
